6484ea90fd8444b3c0b4ff45eeb082362221a2cb
Jannik Straube <info@jannik-straube.de>
2023-08-24 12:41:03 +0200

    Adjust out

--- out/extend-query.js
+++ out/extend-query.js
@@ -18,8 +18,8 @@ module.exports = function (mongoose, cache) {
     const key = this._key || this.getCacheKey();
     const ttl = this._ttl;
     const isCount = ['count', 'countDocuments', 'estimatedDocumentCount'].includes(this.op);
-    const isLean = this._mongooseOptions.lean;
-    const model = this.model.modelName;
+    // const isLean = this._mongooseOptions.lean;
+    // const model = this.model.modelName;
 
     return new Promise((resolve, reject) => {
       cache.get(key, (err, cachedResults) => {
